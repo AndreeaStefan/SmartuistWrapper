25752b7da6b51463dc5503b0878f58a1b78c3bc5
unknown <hubert.zajac@outlook.com>
2019-05-15 17:17:33 +0200

    files

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using InputTracking = UnityEngine.XR.InputTracking;
+
+public class FollowCamera : MonoBehaviour
+{
+    public  GameObject target;
+    void Start() 
+    {
+        InputTracking.disablePositionalTracking = false;
+        var rotation = Helper.getYAxisRotation(transform.rotation);
+//        target.transform.localRotation = Quaternion.Inverse(rotation);
+        var transform1 = transform;
+        var newPosition = transform1.position + transform1.forward * 2;
+        newPosition.y = 0;
+        target.transform.position = newPosition;
+        target.transform.rotation = rotation;
+    }
+
+}
---
+++ Assets/Scripts/TrackerPositionHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackerPositionHandler : MonoBehaviour
+{
+    public GameObject target;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        var newPosition = transform.position;
+        newPosition.y = target.transform.position.y;
+        target.transform.position = newPosition;
+
+    }
+}
